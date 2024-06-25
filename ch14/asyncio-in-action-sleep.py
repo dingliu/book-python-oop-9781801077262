1df0e785a20ce09181cee626f50912ef94a57e16
Ding Liu <contact@dingliu.me>
2024-06-24 23:11:27 -0400

    Update ch14 asyncio

--- ch14/asyncio-in-action-sleep.py
+++ ch14/asyncio-in-action-sleep.py
@@ -1,11 +1,13 @@
 import asyncio
 import random
 
+
 async def random_sleep(counter: int) -> None:
     delay = random.random() * 5
     print(f"[{counter}] Sleeping for {delay:.2f} seconds.")
     await asyncio.sleep(delay)
     print(f"[{counter}] awakens, refreshed.")
+
 
 async def sleepers(how_many: int) -> None:
     print(f"Creating [{how_many}] random sleepers.")
@@ -14,6 +16,7 @@ async def sleepers(how_many: int) -> None:
 
     print(f"Waiting for [{how_many}] random sleepers to wake up.")
     await asyncio.gather(*tasks)
+
 
 if __name__ == "__main__":
     asyncio.run(sleepers(5))
